d68637b0a84967f355f54f38d8dc811c8eab89d4
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-07-22 11:23:08 +0400

    nhbh

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month < 1 || month > 12) {
+            System.out.println("Wrong number! The month number should be between 1 and 12.");
+        } else {
+            int daysInMonth = getDaysInMonth(month);
+            System.out.println("Number of days in month " + month + ": " + daysInMonth);
+        }
+    } private int getDaysInMonth(int monthNumber) {
+        switch (monthNumber) {
+            case 2: // February
+                return 28; // For simplicity, we assume non-leap year.
+            case 4: // April
+            case 6: // June
+            case 9: // September
+            case 11: // November
+                return 30;
+            default:
+                return 31;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double taxRate;
+        if (salary <= 0) {
+            System.out.println("Wrong input!");
+            return;
+        } else if (salary <= 10000) {
+            taxRate = 0.15;
+        } else if (salary <= 20000 && salary > 10000) {
+            taxRate = 0.18;
+        } else {
+            taxRate = 0.20;
+        }
     }
 }
